db741e828238f8239eadbce36e079116a962cc5b
y724u <68385107+y724u@users.noreply.github.com>
2020-12-22 14:46:00 +0900

    必要な情報を変数に格納

--- script.js
+++ script.js
@@ -15,21 +15,51 @@ $(function () {
     let $id = $('#id');
     const API_KEY = '';
     $.ajax({
-      url: `https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=${API_KEY}&address=${$targetArea}&name=${$targetStore}`,
+      url: `https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=${API_KEY}&address=${$targetArea}&name=${$targetStore}&private_room=1`,
+      // private_room=1 は絞り込みあり、０はなし
+
       type: 'GET',
       dataType: 'json',
+
       // フォーム要素の内容をハッシュ形式に変換
       data: $('form').serializeArray(),
       timeout: 5000,
     })
       .done(function (data) {
         // 通信成功時の処理を記述
-        $.each(data.rest , function(i , id){
-          $id.append('<li>' + i + '<li>');
-        });
-        // console.log(JSON.stringify(data.rest));
-        const test = (data.rest);
-        console.log(test);
+
+        // 検索結果件数
+        let $hitCounts = data.total_hit_count;
+
+        for (let index = 0; index < data.rest.length; index++) {
+          let element = data.rest[index];
+          console.log(element);
+
+          // 店名
+          let $restName = element.name;
+          // console.log($restName);
+
+          // 最寄駅
+          let $station = element.access.station;
+          // console.log($station);
+
+          // 駅から何分
+          let $walk = element.access.walk;
+          // console.log($walk);
+
+          // 店舗案内文
+          let $pr = element.pr.pr_short;
+          // console.log($pr);
+
+          // URL
+          let $url = element.url;
+          // console.log($url);
+
+          // 店舗画像
+          let $image = element.image_url.shop_image1;
+          // console.log($image);
+        }
+
       })
       .fail(function () {
         // 通信失敗時の処理を記述
