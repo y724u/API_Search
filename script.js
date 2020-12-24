9370be058c1d48a20790dc24229d4e499cdb9a61
y724u <68385107+y724u@users.noreply.github.com>
2020-12-24 11:35:46 +0900

    説明を追記

--- script.js
+++ script.js
@@ -2,49 +2,42 @@
 // 検索ボタンがsubmitされたら
 $(function () {
   $('.js-form').on('submit', function () {
+
     // エリア、店舗名、個室があるか、valを取得する
     const $targetArea = $('.js-area').val();
     const $targetStore = $('.js-store').val();
+
+    // 個室ありにチェックがされているかprivate_room=1 は絞り込みあり、0はなし
     const $checkedBox = $('.js-pvRoom').filter(':checked');
 
     // Ajax通信を開始
-    let $id = $('#id');
     const API_KEY = '';
     $.ajax({
       url: `https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=${API_KEY}&address=${$targetArea}&name=${$targetStore}&private_room=${$checkedBox.length}`,
-      // private_room=1 は絞り込みあり、０はなし
-
       type: 'GET',
       dataType: 'json',
-
       // フォーム要素の内容をハッシュ形式に変換
       data: $('form').serializeArray(),
       timeout: 5000,
     })
       .done(function (data) {
         // 通信成功時の処理を記述
-
         // 検索結果件数
         let $hitCounts = data.total_hit_count;
-        console.log($hitCounts);
-
-        let $showedPage = $('.result__item').length + 10;
-        console.log($showedPage);
-
-        if ($('.result__page').length) {
-
-          $('.result__page').replaceWith(`<p class='result__page'>1~${$showedPage}件を表示 / 全${$hitCounts}件</p>`);
+        // 現在表示しているページを表示
+        let $showedPages = $('.result__item').length + 10;
+        // (.result__page)を格納
+        const $result = $(`<p class='result__page js-result'>1~${$showedPages}件を表示 / 全${$hitCounts}件</p>`)
+        if ($('.js-result').length) {
+          $('.js-result').replaceWith($result);
         } else {
-          $('.result__container').prepend(`<p class='result__page'>1~${$showedPage}件を表示 / 全${$hitCounts}件</p>`);
-
-          $('.result__container').append(`<p class='result__page'>1~${$showedPage}件を表示 / 全${$hitCounts}件</p>`);
-        }
-
-
+          $('.result__container').append($result);
+          $('.result__container').prepend(`<p class='result__page js-result'>1~${$showedPages}件を表示 / 全${$hitCounts}件</p>`);
+        };
+        // データ取り出し
         for (let index = 0; index < data.rest.length; index++) {
           let element = data.rest[index];
           // console.log(element);
-
 
           // 店名
           let $restName = element.name;
@@ -72,6 +65,7 @@ $(function () {
           let $category = element.category;
           // console.log($category);
 
+          // 検索結果を表示
           $('.result__list').append(
             `<li class="result__item">
             <div class="result__image">
@@ -83,25 +77,21 @@ $(function () {
             </dt>
             <dd class="result__location">${$station} 徒歩${$walk}分 / ${$category}</dd>
             <dd class="result__description">${$pr}</dd>
-            <dd class="result__privateRoom">完全個室</dd>
+
             </dl>
             </li>`);
+          // <dd class="result__privateRoom">完全個室</dd>
         }
 
+        // if(($checkedBox.length) === 1) {
+        //   console.log('ok');
+        //   $('.result__description').append('<dd class="result__privateRoom">完全個室</dd>');
+        // }else if ($('result__privateRoom').length===0){
+        // };
       })
       .fail(function () {
         // 通信失敗時の処理を記述
         console.log('fail');
       });
-
-    // 条件に当てはまる件数を取得&表示
-
-
-
-    // 取得した情報に当てはまる店舗情報（店舗画像、最寄駅から何分か、お店のカテゴリ、店舗紹介文、個室があるか否か）を１０件ずつ表示
-
-
-
-    // 表示された情報をクリックされたら店舗ページへ別ウインドウへ飛ばす
   })
 });
