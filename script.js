54a6f91cbaf223b5270dadbb9b500249ceb7dad8
y724u <68385107+y724u@users.noreply.github.com>
2020-12-30 12:15:40 +0900

    変数名を変更

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // API_KEY
-const API_KEY = '';
+const API_KEY = 'b448e3ec0071bee3486473b06e574985';
 // 検索ボタンがsubmitされたら
 $(function () {
   $('.js-form').on('submit', function (e) {
@@ -30,50 +30,50 @@ $(function () {
     // 通信成功時の処理を記述
       .done(function (data) {
         // 検索結果件数
-        const $hitCounts = data.total_hit_count;
+        const hitCounts = data.total_hit_count;
         // 現在表示しているページを表示
-        const $showedPages = $('.js-item').length + 20;
+        const showedPages = $('.js-item').length + 20;
         // (.result__page)を格納
-        const $result = $(`<p class='result__page js-result'>1~${$showedPages}件を表示 / 全${$hitCounts}件</p>`)
+        const $result = $(`<p class='result__page js-result'>1~${showedPages}件を表示 / 全${hitCounts}件</p>`)
         if ($('.js-result').length) {
           $('.js-result').replaceWith($result);
         } else {
           $('.js-container').append($result);
           // $resultが使えない
-          $('.js-container').prepend(`<p class='result__page js-result'>1~${$showedPages}件を表示 / 全${$hitCounts}件</p>`);
+          $('.js-container').prepend(`<p class='result__page js-result'>1~${showedPages}件を表示 / 全${hitCounts}件</p>`);
         };
         // データ取り出し
         for (let index = 0; index < data.rest.length; index++) {
           let element = data.rest[index];
           // 店名
-          let $restName = element.name;
+          let restName = element.name;
           // 最寄駅
-          let $station = element.access.station;
+          let station = element.access.station;
           // 駅から何分
-          let $walk = element.access.walk;
+          let walk = element.access.walk;
           // 店舗案内文
-          let $pr = element.pr.pr_long;
+          let pr = element.pr.pr_long;
           // 個室
-          let $privateRoom = $pr.indexOf('個室');
+          let privateRoom = pr.indexOf('個室');
           // URL
-          let $url = element.url;
+          let url = element.url;
           // 店舗画像
-          let $image = element.image_url.shop_image1
+          let image = element.image_url.shop_image1
           // カテゴリ
-          let $category = element.category;
-          // 検索結果を表示
-          if ($privateRoom > -1 || isPrivate === 1) {
+          let category = element.category;
+        //   // 検索結果を表示
+          if (privateRoom > -1 || isPrivate === 1) {
             $('.js-list').append(
               `<li class="result__item js-item">
                 <div class="result__image">
-                  <img src="${$image}" alt="店舗イメージ">
+                  <img src="${image}" alt="店舗イメージ">
                 </div>
                 <dl class="result__content">
                   <dt class="result__storeName">
-                    <a href="${$url}" " target="_blank" rel="noopener noreferrer">${$restName}</a>
+                    <a href="${url}" " target="_blank" rel="noopener noreferrer">${restName}</a>
                   </dt>
-                  <dd class="result__location">${$station} 徒歩${$walk}分 / ${$category}</dd>
-                  <dd class="result__description">${$pr}</dd>
+                  <dd class="result__location">${station} 徒歩${walk}分 / ${category}</dd>
+                  <dd class="result__description">${pr}</dd>
                   <dd class="result__privateRoom">完全個室</dd>
                 </dl>
               </li>`);
@@ -81,18 +81,19 @@ $(function () {
             $('.js-list').append(
               `<li class="result__item js-item">
                 <div class="result__image">
-                  <img src="${$image}" alt="店舗イメージ">
+                  <img src="${image}" alt="店舗イメージ">
                 </div>
                 <dl class="result__content" id='js'>
                   <dt class="result__storeName">
-                    <a href="${$url}" " target="_blank" rel="noopener noreferrer">${$restName}</a>
+                    <a href="${url}" " target="_blank" rel="noopener noreferrer">${restName}</a>
                   </dt>
-                  <dd class="result__location">${$station} 徒歩${$walk}分 / ${$category}</dd>
-                  <dd class="result__description js-description">${$pr}</dd>
+                  <dd class="result__location">${station} 徒歩${walk}分 / ${category}</dd>
+                  <dd class="result__description js-description">${pr}</dd>
                 </dl>
               </li>`);
           }
         }
+
         // 店舗紹介文...で表示する(.js-description)
         $(function() {
           $('.js-description , .result__description').each(function() {
